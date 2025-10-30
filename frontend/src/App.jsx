062b4dd4ca8995b5254f7c0de9b96ec87e06d600
knightowlrehan <rehan@knightowl.online>
2025-10-31 01:13:37 +0530

    Update App component to use Layout with HeroSection

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Layout from './components/layout/Layout';
+import HeroSection from './components/sections/HeroSection';
+
+function App() {
+  return (
+    <Layout>
+      <HeroSection />
+    </Layout>
+  );
+}
+
+export default App
